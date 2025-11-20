baac5e9388798b885b89fd9bdb28b73ae61f86be
Davidson Mochahari <davidsonmochahari06@gmail.com>
2025-11-20 19:54:15 +0530

    Added loading

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Loading() {
+    return (
+        <div className="w-full h-screen flex items-center justify-center">
+            <div className="animate-spin rounded-full h-16 w-16 border-4 border-fuchsia-400 border-t-transparent"></div>
+        </div>
+    );
+}

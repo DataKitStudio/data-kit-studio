8fdc2c76149f9845d4c17267b4936a0dcb7245c7
Davidson Mochahari <davidsonmochahari06@gmail.com>
2025-11-24 20:15:34 +0530

    Fixed error in services

--- app/services/components/services-section.tsx
+++ app/services/components/services-section.tsx
@@ -4,13 +4,13 @@ import ServicesCard from "./helper-components/services-card"
 
 
 export default function ServicesSection() {
-    return(
+    return (
         <div className="grid lg:grid-cols-3 grid-cols-1 xl:px-40 lg:px-20 py-10 px-10 gap-8">
-            {
-                servicesData.map((service, index:number) =>(
-                    <ServicesCard key={service.key} image={service.image} title={service.title} description={service.description} services={service.services} />
+            {/* {
+                servicesData.map((service, index: number) => (
+                    // <ServicesCard key={service.key} image={service.image} title={service.title} description={service.description} services={service.services} />
                 ))
-            }
+            } */}
         </div>
     )
 }
